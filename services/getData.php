a249149d3f2e878dbae1d12ed16d3f52d77399b6
eyl407 <eloh@stanford.edu>
2023-01-24 11:14:52 -0800

    Eloh parallel UI (#11)
    * parallelize collection window queries and track progress for cw queries through separate progress bars.
    
    * clean up debug messages
    
    * parallelize collection window queries and track progress for cw queries through separate progress bars.
    
    * clean up debug messages
    
    * - add check to make sure project is in production status before retrieving data
    - add logging at start and end of duster get data
    
    * - delete unneeded class file
    
    * - remove debug messages

--- services/getData.php
+++ services/getData.php
@@ -1,6 +1,7 @@
 <?php
 namespace Stanford\Duster;
 /** @var \Stanford\Duster\Duster $module */
+use REDCap;
 
 require_once $module->getModulePath() . "classes/DusterConfigClass.php";
 require_once $module->getModulePath() . "classes/RedcapToStarrLinkConfig.php";
@@ -9,9 +10,21 @@ $pid = isset($_GET['pid']) && !empty($_GET['pid']) ? $_GET['pid'] : null;
 $action = isset($_GET['action']) && !empty($_GET['action']) ? $_GET['action'] : null;
 
 $rtoslink_config = new RedcapToStarrLinkConfig($pid, $module);
-$return_obj = [];
+$return_obj = array();
 
-if ($action === 'cohort') {
+if ($action === 'projectStatus') {
+    $result = $module->query(
+        "select `status` from redcap_projects where `project_id`=?",
+        [$pid]);
+    if ($result->num_rows > 0) {
+        $return_obj['status'] = 200;
+        $return_obj['production_mode'] = json_decode($result->fetch_assoc()['status'], true);
+    } else {
+        $return_obj['status'] = 400;
+        $return_obj['message'] = 'Unable to verify project status';
+    }
+} else if ($action === 'cohort') {
+    REDCap::logEvent("DUSTER: Get Data Start");
     $duster_config = new DusterConfigClass($pid, $module);
     $return_obj = $duster_config->getDusterRequestObject();
     $return_obj["queries"]= $rtoslink_config->getQueries();
@@ -33,7 +46,7 @@ if ($action === 'cohort') {
     $query = isset($_GET['query']) && !empty($_GET['query']) ? json_decode($_GET['query'], true) : null;
     //$module->emDebug('$query = ' . $query);
     $resp = $rtoslink_config->updateData($query['query_name']);
-    $query_label = ($query==null)? 'All' : $query['query_label'];
+    $query_label = ($query == null) ? 'All' : $query['query_label'];
     //$module->emDebug('$resp = ' . $resp);
     if ($resp) {
         $return_obj['status'] = 200;
@@ -42,9 +55,12 @@ if ($action === 'cohort') {
         $return_obj['status'] = 400;
         $return_obj['message'] = "Error: Unable to update $query_label";
     }
+} else if ($action === 'complete') {
+    REDCap::logEvent("DUSTER: Get Data Complete");
+    $return_obj['status'] = 200;
 } else {
     $return_obj['status'] = 400;
     $return_obj['message'] = "Unrecognized action: $action";
 }
 echo json_encode($return_obj);
-?>
+
