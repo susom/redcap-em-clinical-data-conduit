4cd50116359f26fe9cfba772fed6c9a96e996c46
roquej <38173852+roquej@users.noreply.github.com>
2023-09-26 12:47:33 -0700

    update logging for IRB check

--- services/checkIRB.php
+++ services/checkIRB.php
@@ -7,7 +7,7 @@ namespace Stanford\Duster;
  */
 
 $irb_num = $_POST["project_irb_number"];
-$module->emLog("Looking up IRB number $irb_num.");
+// $module->emLog("Looking up IRB number $irb_num."); // redundant logging
 $irb_status = false;
 $irb_status_string = 'false';
 // $irb_all = [];
@@ -16,17 +16,17 @@ try {
     $IRB = \ExternalModules\ExternalModules::getModuleInstance('irb_lookup');
     $irb_status = $IRB->isIRBValid($irb_num);
     $irb_status_string = $irb_status === true ? 'true' : 'false';
-    $module->emLog("Status for IRB number {$irb_num} returned by IRB Lookup EM: {$irb_status_string}.");
+    $module->emLog("Status for IRB number $irb_num returned by IRB Lookup EM: $irb_status_string.");
     // $status = $IRB->isIRBValid($irb_num) ? true : false;
     //$irb_privacy = $IRB->getPrivacySettingsV2($irb_num);
     //$irb_all = $IRB->getAllIRBData($irb_num);
 
 } catch(Exception $ex) {
-    $module->emError("Exception when creating an instance of the IRB Lookup EM.");
+    $module->emError("Exception when creating an instance of the IRB Lookup EM to look up IRB number $irb_num: $ex");
 }
 //$module->emLog("IRB status for $irb_num: $status");
 // $module->emLog("IRB privacy " . print_r($irb_privacy, true));
 //$module->emLog("IRB all " . print_r($irb_all, true));
-$module->emLog("Returning {$irb_status_string} status for IRB number {$irb_num}.");
+// $module->emLog("Returning {$irb_status_string} status for IRB number {$irb_num}."); // redundant logging
 echo $irb_status;
 ?>
