a383b7d63ce1dcdf48cbb053d8351c57662d0f5b
roquej <38173852+roquej@users.noreply.github.com>
2024-01-03 15:43:23 -0800

    Add html to project-level webpage for launching editor mode

--- pages/editProject.php
+++ pages/editProject.php
@@ -5,5 +5,20 @@ namespace Stanford\Duster;
 /**
  * entrypoint to DUSTER's edit project UI (Vue app)
  */
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<h1>
+  DUSTER: Edit Project
+</h1>
+<p>Hitting the button below will launch an application where you may perform the following modifications to your project:</p>
+<ol>
+  <li>Add new Researcher-Provided Information.</li>
+  <li>Select additional demographics.</li>
+  <li>Add new data collection windows.</li>
+  <li>Add new clinical variables to pre-existing data collection windows.</li>
+  <li>Select additional aggregations to pre-existing clinical variables in data collection windows.</li>
+</ol>
+<button type="button">Launch Editor</button>
+</html>
