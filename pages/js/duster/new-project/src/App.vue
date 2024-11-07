4070c66d3fc5b4c5c5e678c4a871abbbbae5f3ed
Jonasel Roque <38173852+roquej@users.noreply.github.com>
2024-11-07 06:02:11 -0800

    retroactive support for user-defined labs feature for older projects (#97)
    * retroactive support for user-defined labs feature for older projects, temporary debugging printouts
    
    * remove debugging printouts

--- pages/js/duster/new-project/src/App.vue
+++ pages/js/duster/new-project/src/App.vue
@@ -469,8 +469,6 @@ const getDusterMetadata = (metadataUrl:string) => {
     scoreOptions.value = resp.data.scores;
     clinicalDateOptions.value = resp.data.clinical_dates;
 
-    // get lab results metadata
-
   } else {
     axios.get(metadataUrl)
       .then(function (response) {
@@ -514,6 +512,13 @@ const getDusterMetadata = (metadataUrl:string) => {
 };
 
 const loadEditMode = () => {
+
+  // add an empty array for missing user-defined labs to each data collection window
+  // retroactive support for user-defined labs feature for older projects
+  projectConfig.initial_design.collectionWindows.forEach((cw: any) => {
+    cw.data.ud_labs = cw.data.ud_labs ? cw.data.ud_labs : [];
+  });
+
   initialDesign.value = projectConfig.initial_design;
 
   // transform and load researcher-provided data
